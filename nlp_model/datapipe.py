ec0cbc7128af853ee166d0ff3edba93d63414bc1
Xin Yu <xinyu_03@hotmail.com>
2023-06-21 04:47:37 +0800

    trained model

---
+++ nlp_model/datapipe.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import torch
+from torchdata.datapipes.iter import IterableWrapper, IterDataPipe
+from transformers import AutoTokenizer
+
+class NewsDataPipe(IterDataPipe):
+    '''
+    DataPipe for loading news data from S3
+
+    Args:
+        s3_url (str): S3 URL to load data from
+        tokenizer (transformers.PreTrainedTokenizer): Tokenizer to use
+        num_files (int): Number of files to load
+    '''
+    def __init__(self, s3_url: str, tokenizer: AutoTokenizer, num_files: int):
+        super().__init__()
+        self.url_wrapper = IterableWrapper(s3_url).list_files_by_s3().shuffle().sharding_filter()
+        self.tokenizer = tokenizer
+        self.num_files = num_files
+
+    def __iter__(self):
+        for _, file in self.url_wrapper:
+            temp = pd.read_csv(file)
+            label = torch.from_numpy(temp['outcome'].values)
+            # For BERT model
+            bert_input = []
+            embedded = [self.tokenizer(t, padding='max_length', max_length=100, truncation=True, return_tensors='pt') for t in temp['headlines']]
+            bert_input.append(torch.cat([e['input_ids'] for e in embedded], dim=0))
+            bert_input.append(torch.cat([e['attention_mask'] for e in embedded], dim=0))
+
+            # Tabular features
+            tabular_input = [torch.from_numpy(temp[col].values).to(torch.float32).squeeze() for col in temp.columns if col not in ['outcome', 'headlines']]
+            yield bert_input, tabular_input, label
+
+    def __len__(self):
+        return self.num_files
+    
---
+++ nlp_model/model.py
@@ -0,0 +1,76 @@
+import optuna
+import torch
+from transformers import BertModel
+
+param_type_to_suggestion_map = {
+    'int': optuna.trial.suggest_int,
+    'float': optuna.trial.suggest_float,
+    'choice': optuna.trial.suggest_categorical,
+    'loguniform': optuna.trial.suggest_loguniform,
+    'discrete_uniform': optuna.trial.suggest_discrete_uniform
+}
+
+class ModelConstructor:
+    '''
+    '''
+    def __init__(self, config: dict, device: str='cpu'):
+        self.device = device
+        self.model = None 
+        self.experiment_id = config['experiment_id']
+        self.experiment_name = config['experiment_name']
+        self.parameters = config['parameters']
+
+
+    def create_model(self, trial: optuna.Trial):
+        pass
+
+    def _get_suggestions(self):
+        fun = param_type_to_suggestion_map[self.parameters['type']]
+
+
+    def save_model(self):
+        pass
+
+    def load_model(self):
+        pass
+
+    def train(self, trial: optuna.Trial):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def upload_model(self):
+        pass
+        
+
+
+class FakeNewsModel(torch.nn.module):
+    def __init__(self, model_id: int, dropout1: float=0.25, dropout2: float=0.25, hidden_size: int=12):
+        self.model_id = model_id
+        # define layers
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        self.dropout_1 = torch.nn.Dropout(dropout1)
+        self.linear_1 = torch.nn.Linear(768, hidden_size)
+        self.dropout_2 = torch.nn.Dropout(dropout2)
+        self.linear_2 = torch.nn.Linear(hidden_size + 20, 1)
+        self.normalize = torch.nn.functional.normalize
+        self.relu = torch.nn.ReLU()
+        self.sigmoid = torch.nn.Sigmoid()
+
+
+    def forward(self, bert_input: dict, tabular_input: list):
+        _, pooled_output = self.bert(**bert_input)
+        dropout_1_output = self.dropout_1(pooled_output)
+        linear_1_output = self.linear_1(dropout_1_output)
+        relu_output = self.relu(linear_1_output)
+        norm1 = self.normalize(relu_output, p=2, dim=1)
+        norm2 = self.normalize(tabular_input, p=2, dim=1)
+        combined_output = torch.cat([norm1, norm2], dim=1)
+        dropout_2_output = self.dropout_2(combined_output)
+        linear_2_output = self.linear_2(dropout_2_output)
+        return self.sigmoid(linear_2_output)
+    
