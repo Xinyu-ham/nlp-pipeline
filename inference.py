e998b26e4aecdd323abc4f1c4c0db059d0dba81a
Xin Yu <xinyu_03@hotmail.com>
2023-07-01 05:27:24 +0800

    added inference

--- inference.py
+++ inference.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from nlp_model import Trainer
+from nlp_model import FakeNewsInference
 
 app = FastAPI()
 
-# @app.get("/")
+@app.post("/")
+async def predict(headline: str):
+    model = Inference()
+    return model.predict(headline)
